b670e92b60bc9454a7fbc4104a51aa860084a791
igor <embiggenerd@gmail.com>
2019-12-11 13:53:51 -0500

    api call working, loading screen working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+
+import LoadingScreen from "./components/LoadingScreen"
 import "./App.css";
 
 function App() {
+
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    // Update the document title using the browser API
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then(req => console.log(req.data))
+      .then(() => setLoading(false))
+      .catch(e => console.log(e))
+  }, []);
+
+  if (loading) {
+    return <LoadingScreen />
+  }
   return (
     <div className="App">
-      <p>
+      <span>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+        app! Have fun !
+      </span>
     </div>
   );
 }
---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function LoadingScreen() {
+
+    return (
+        <div className="LoadingScreen">
+            <span>
+                Loading
+            </span>
+        </div>
+    );
+}
+
+export default LoadingScreen;
