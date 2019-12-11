b7f4c837808123795f8c76672b16c476f8693a6f
igor <embiggenerd@gmail.com>
2019-12-11 14:20:23 -0500

    date, explanation, image props and state working

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios'
 
 import LoadingScreen from "./components/LoadingScreen"
+import PlanetInfo from "./components/PlanetInfo"
 import "./App.css";
+
 
 function App() {
 
   const [loading, setLoading] = useState(true)
+  const [date, setDate] = useState("")
+  const [explanation, setExplanation] = useState("")
+  const [imgURL, setImgURL] = useState("")
+
   useEffect(() => {
     // Update the document title using the browser API
-    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-      .then(req => console.log(req.data))
+    const str = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14'
+    axios.get(str)
+      .then(req => {
+        console.log(req.data)
+        setDate(req.data.date)
+        setExplanation(req.data.explanation)
+        setImgURL(req.data.hdurl)
+      })
       .then(() => setLoading(false))
       .catch(e => console.log(e))
   }, []);
@@ -18,12 +30,13 @@ function App() {
   if (loading) {
     return <LoadingScreen />
   }
+
   return (
     <div className="App">
-      <span>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun !
-      </span>
+      <PlanetInfo
+        exp={explanation}
+        date={date}
+        url={imgURL} />
     </div>
   );
 }
