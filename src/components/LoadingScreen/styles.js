ef9fc800fc5c547671e80e09c5b0df41a29b8a95
igor <embiggenerd@gmail.com>
2019-12-12 18:36:56 -0500

    styling changed over to styled components

---
+++ src/components/LoadingScreen/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const LoadingScreenContainer = styled.div`
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    font-size: 2rem;
+    background: #ff7b00;
+    color: white;
+`
---
+++ src/components/PlanetInfo/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+export const PlanetInfoContainer = styled.div`
+    background: #ff7b00;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+`
+
+export const PlanetExplanation = styled.p`
+    width: 500px;
+`
+
+export const PlanetImgContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 1000px;
+    margin: 100px auto 100px auto;
+`
+
+export const PlanetImg = styled.img`
+    width: 500px;
+    height: 500px;
+`
+
+export const PlanetDate = styled.div`
+
+`
